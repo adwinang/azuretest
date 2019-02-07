ac7083c104df24b89a984b1d30619899f4aa0493
Ang Yong Xiang Adwin <s10178499@connect.np.edu.sg>
2019-02-08 01:26:53 +0800

    Add project files.

---
+++ testSQL/Index.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="testSQL.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:Label ID="Label1" runat="server" Text="Input Name"></asp:Label>
+            <br />
+            <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="btnName" runat="server" OnClick="btnName_Click" Text="Send" />
+            <br />
+            <asp:Label ID="lblInfo" runat="server"></asp:Label>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ testSQL/Index.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace testSQL
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnName_Click(object sender, EventArgs e)
+        {
+            string cntString = "Server=tcp:relevium-p2.database.windows.net,1433;Initial Catalog=relevium;Persist Security Info=False;User ID=relevium;Password=Pr0jectRelev1um;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            SqlConnection conn = new SqlConnection(cntString);
+            try
+            {
+                conn.Open();
+                string sql = String.Format("INSERT INTO TestData(Name) VALUES(@name)");
+
+                SqlCommand cmd = new SqlCommand("INSERT INTO TestData (Name) " + "OUTPUT INSERTED.TestID " + "VALUES ( @name )", conn);
+                cmd.Parameters.AddWithValue("@name", txtName.Text);
+                int res = cmd.ExecuteNonQuery();
+
+                lblInfo.Text = res.ToString();
+
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
